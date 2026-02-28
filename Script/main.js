ed889614488a317abe0c760c12088176e1abf4f0
SimoneDiMaria17 <simonedimaria92.sdm@gmail.com>
2026-02-28 18:19:32 +0100

    alcune canzoni

--- Script/main.js
+++ Script/main.js
@@ -8,3 +8,77 @@ function diable(id){
     }
 }
 
+const canzoni=[
+    {
+        "American_Rap":[
+           { "url":"../Canzoni/10_Categorie/american Rap/__spotdown.org.mp3"},
+            {"url":"../Canzoni/10_Categorie/american Rap/A BOY IS A GUN__spotdown.org.mp3"},
+            {"url":"../Canzoni/10_Categorie/american Rap/I Wonder.mp3"},
+            {"url":"../Canzoni/10_Categorie/american Rap/PRIDE._spotdown.org.mp3"},
+            {"url":"../Canzoni/10_Categorie/american Rap/it boy.mp3"}
+        ],
+        "Anni_10_Ita":[
+           { "url":"../Canzoni/10_Categorie/Anni 10 Ita"},
+            {"url":"../Canzoni/10_Categorie/Anni 10 Ita"},
+            {"url":"../Canzoni/10_Categorie/Anni 10 Ita"},
+            {"url":"../Canzoni/10_Categorie/Anni 10 Ita"},
+            {"url":"../Canzoni/10_Categorie/Anni 10 Ita"}
+        ],
+        "Anni_10 Mondo":[
+           { "url":"../Canzoni/10_Categorie"},
+            {"url":"../Canzoni/10_Categorie"},
+            {"url":"../Canzoni/10_Categorie"},
+            {"url":"../Canzoni/10_Categorie"},
+            {"url":"../Canzoni/10_Categorie"}
+        ],
+        "Bilions":[
+           { "url":"../Canzoni/10_Categorie"},
+            {"url":"../Canzoni/10_Categorie"},
+            {"url":"../Canzoni/10_Categorie"},
+            {"url":"../Canzoni/10_Categorie"},
+            {"url":"../Canzoni/10_Categorie"}
+        ],
+        "Film":[
+           { "url":"../Canzoni/10_Categorie"},
+            {"url":"../Canzoni/10_Categorie"},
+            {"url":"../Canzoni/10_Categorie"},
+            {"url":"../Canzoni/10_Categorie"},
+            {"url":"../Canzoni/10_Categorie"}
+        ],
+        "Ita_rap":[
+           { "url":"../Canzoni/10_Categorie"},
+            {"url":"../Canzoni/10_Categorie"},
+            {"url":"../Canzoni/10_Categorie"},
+            {"url":"../Canzoni/10_Categorie"},
+            {"url":"../Canzoni/10_Categorie"}
+        ],
+        "Italia_indie":[
+           { "url":"../Canzoni/10_Categorie"},
+            {"url":"../Canzoni/10_Categorie"},
+            {"url":"../Canzoni/10_Categorie"},
+            {"url":"../Canzoni/10_Categorie"},
+            {"url":"../Canzoni/10_Categorie"}
+        ],
+        "Lol_rap":[
+           { "url":"../Canzoni/10_Categorie"},
+            {"url":"../Canzoni/10_Categorie"},
+            {"url":"../Canzoni/10_Categorie"},
+            {"url":"../Canzoni/10_Categorie"},
+            {"url":"../Canzoni/10_Categorie"}
+        ],
+        "Mistery":[
+           { "url":"../Canzoni/10_Categorie"},
+            {"url":"../Canzoni/10_Categorie"},
+            {"url":"../Canzoni/10_Categorie"},
+            {"url":"../Canzoni/10_Categorie"},
+            {"url":"../Canzoni/10_Categorie"}
+        ],
+        "Sanremo":[
+           { "url":"../Canzoni/10_Categorie"},
+            {"url":"../Canzoni/10_Categorie"},
+            {"url":"../Canzoni/10_Categorie"},
+            {"url":"../Canzoni/10_Categorie"},
+            {"url":"../Canzoni/10_Categorie"}
+        ],
+    }
+]
